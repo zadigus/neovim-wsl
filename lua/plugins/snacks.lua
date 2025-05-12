e36da6c1a36d11ea79105e6b4b3334c74714342e
Laurent Michel <laurent.michel@cognex.com>
2025-05-12 21:05:45 +0200

    enable grep search in hidden and ignored files

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,6 +3,8 @@ return {
     "folke/snacks.nvim",
     opts = {
       picker = {
+        hidden = true,
+        ignored = true,
         sources = {
           files = {
             hidden = true,
