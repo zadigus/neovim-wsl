4897e5b281921d3c1d25cbdb70a6455febdc6ed8
Laurent Michel <laurent.michel@cognex.com>
2026-01-22 13:13:59 +0100

    configure ty

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -8,22 +8,28 @@ return {
           mason = false,
           autostart = false,
         },
-        pylsp = {
-          -- cf. https://github.com/python-lsp/python-lsp-server/blob/develop/CONFIGURATION.md
+        ty = {
+          enabled = true,
           settings = {
-            pylsp = {
-              plugins = {
-                pycodestyle = { enabled = true, ignore = { "E501", "W503" } },
-                flake8 = { enabled = true, ignore = { "E501", "W503" } },
-                mypy = { enabled = true },
-                isort = { enabled = true },
-                rope_autoimport = { enabled = true },
-                rope_completion = { enabled = true },
+            ty = {
+              diagnosticMode = "workspace",
+              completions = {
+                autoImport = true,
               },
             },
           },
         },
-        -- for ty, cf. https://www.reddit.com/r/neovim/comments/1kl9qv8/comment/ms0ngxa/?tl=fr&utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button
+        ruff = {
+          init_options = {
+            settings = {
+              logLevel = "error",
+            },
+          },
+        },
+        pylsp = {
+          -- cf. https://github.com/python-lsp/python-lsp-server/blob/develop/CONFIGURATION.md
+          enabled = false,
+        },
         cucumber_language_server = {
           settings = {
             cucumber = {
