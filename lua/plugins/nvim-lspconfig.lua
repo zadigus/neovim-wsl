b03767484f0da80cb5891043ee6be1e9f31e18b4
Laurent Michel <lmichel@Laurents-MBP.pc.cognex.com>
2026-07-06 09:25:04 +0200

    enable semantic token provider on terraform

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -4,15 +4,6 @@ return {
     opts = {
       servers = {
         kotlin_language_server = {},
-        terraformls = {
-          -- Workaround: Neovim 0.12 has an O(n²) regression in vim.str_utfindex()
-          -- that causes 100% CPU when processing large semantic-token responses.
-          -- terraform-ls sends huge token payloads for files with heredocs.
-          -- cf. https://github.com/neovim/neovim/issues/36257
-          on_attach = function(client)
-            client.server_capabilities.semanticTokensProvider = nil
-          end,
-        },
         pyright = {
           mason = false,
           autostart = false,
